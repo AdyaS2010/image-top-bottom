bd2fe034b47f329f6dc37355343042d06a2bda97
AdyaS2010 <adya.sastry@gmail.com>
2024-08-15 09:07:38 -0500

    Update and rename text-to-grade.c to bottomup.c

---
+++ bottomup.c
@@ -0,0 +1,46 @@
+// Copies a BMP file
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bmp.h"
+
+int main(int argc, char *argv[])
+{
+    // Ensure proper usage
+    if (argc != 3)
+    {
+        printf("Usage: copy infile outfile\n");
+        return 1;
+    }
+
+    // Remember filenames
+    char *infile = argv[1];
+    char *outfile = argv[2];
+
+    // Open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 2;
+    }
+
+    // Open output file
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
+        fclose(inptr);
+        printf("Could not create %s.\n", outfile);
+        return 3;
+    }
+
+    // Read infile's BITMAPFILEHEADER
+    BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    // Read infile's BITMAPINFOHEADER
+    BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+    return 0;
+}
