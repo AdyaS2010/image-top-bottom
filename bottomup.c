55cc79da1430bb4ab7d820ec088b102ef3a097f0
AdyaS2010 <adya.sastry@gmail.com>
2024-08-15 09:08:34 -0500

    Update bottomup.c

--- bottomup.c
+++ bottomup.c
@@ -1,4 +1,4 @@
-// Copies a BMP file
+// Copies a BMP file (bmp.h)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -42,5 +42,35 @@ int main(int argc, char *argv[])
     // Read infile's BITMAPINFOHEADER
     BITMAPINFOHEADER bi;
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+    // Flip image from bottom-up to top-down
+    bi.biHeight = -bi.biHeight;
+
+    // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
+    {
+        fclose(outptr);
+        fclose(inptr);
+        printf("Unsupported file format.\n");
+        return 4;
+    }
+
+    // Write outfile's BITMAPFILEHEADER
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+
+    // Write outfile's BITMAPINFOHEADER
+    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+
+    // Determine padding for scanlines
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // Close infile
+    fclose(inptr);
+
+    // Close outfile
+    fclose(outptr);
+
+    // Success
     return 0;
 }
